c5c908ede971e86f04314569c67dcb2a8fcc9200
Daniel Peterson <pet01704@umn.edu>
2019-11-07 15:11:15 -0600

    adding main.c

---
+++ pa3/main.c
@@ -0,0 +1,205 @@
+/*test machine: CSELAB_machine_name * date: mm/dd/yy
+* name: full_name1 , [full_name2]
+* x500: id_for_first_name , [id_for_second_name] */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "header.h"
+// pthread.h included in header.h
+
+void *producer(void *args);
+void *consumer(void *args);
+int totals[26];
+pthread_mutex_t totals_lock;
+pthread_mutex_t llist_lock;
+pthread_mutex_t cond_lock;
+pthread_cond_t new_package;
+int eof = 0;
+
+int main(int argc, char *argv[]){
+
+	//expects between 3 and 5 arguments
+	if (argc < 3 || argc > 5) {
+		printf("Wrong number of arguments.\n");
+		printf("Usage: $ %s #consumer filename [option] [#queue_size]\n",argv[0]);
+		exit(1);
+	}
+
+	//argv[1] is the number of consumers
+	int n_consumers = 0;
+	if ((n_consumers = atoi(argv[1])) <= 0){
+		printf("Invalid #consumer. Given '%s', expected a positive integer.\n",argv[1]);
+		printf("Usage: $ %s #consumer filename [option] [#queue_size]\n",argv[0]);
+		exit(1);
+	}
+
+	//argv[2] is the filename
+	FILE *fptr;
+	if ((fptr = fopen(argv[2], "r")) == NULL){
+		printf("Unable to open the file '%s'.\n",argv[2]);
+		printf("Usage: $ %s #consumer filename [option] [#queue_size]\n",argv[0]);
+		exit(1);       
+	}
+
+	//argv[3] and argv[4] can be option or queue size for ec
+	int option_set = 0;
+	int queue_size_set = 0;
+	int queue_size = 0;
+	for (int i = 3; i < argc; i ++){
+		if (!strcmp(argv[i],"-p") && !option_set){
+			option_set = 1;
+		}else if (!strcmp(argv[i],"-b") && !option_set){
+			option_set = 1;
+		}else if (!strcmp(argv[i],"-bp") && !option_set){
+			option_set = 1;
+		}else if(!queue_size_set && (queue_size = atoi(argv[i]) > 0)){
+			queue_size_set = 1;
+		}else{
+			printf("Invalid argument '%s'.\n",argv[i]);
+			printf("Usage: $ %s #consumer filename [option] [#queue_size]\n",argv[0]);
+			exit(1);   
+		}
+	}
+	
+	
+	pthread_t producer_t;
+	pthread_t consumer_t[n_consumers];
+
+	//create producer thread
+	pthread_create(&producer_t, NULL, producer, fptr); 
+
+	//initialize histogram
+	for(int i =0;i<26;i++){
+		totals[i] = 0;
+	}
+	//initialize mutex lock for totals
+	pthread_mutex_init(&totals_lock, NULL);
+	pthread_mutex_init(&llist_lock, NULL);
+	pthread_mutex_init(&cond_lock, NULL);
+
+	//create n_consumer consumer threads
+	for (int i = 0; i < n_consumers; i++){
+		pthread_create(&consumer_t[i], NULL, consumer, NULL); 
+	}
+	
+	//wait for all threads to join back
+	pthread_join(producer_t, NULL); 
+	for (int i = 0; i < n_consumers; i++){
+		pthread_join(consumer_t[i], NULL); 
+	}
+
+	//close fptr
+	fclose(fptr);
+	
+	//format histogram from int array and write to file
+	FILE *out;
+	out = fopen("result.txt","w+");
+	for(int i =0;i<26;i++){
+		fprintf(out,"%c: %d\n",i+97,totals[i]);
+	}
+	fclose(out);
+
+
+
+	return 0;
+}
+
+void count_words( char *str, int *totals){
+	int reset = 1;
+	int i = 0;
+	while (str[i] != '\0') {
+		char cur = tolower(str[i]);
+   		if (cur >= 'a' && cur <= 'z') {
+			if (reset){
+				totals[cur - 'a']++;
+				reset = 0;
+			}
+		}else{
+			reset = 1;
+		}
+    		i++;
+	}
+}
+
+void *consumer(void *args){
+	int empty = 1;
+
+	while (! (eof && empty) ){
+		//while linked list is empty
+		while (empty){
+			printf("here1\n");
+			pthread_cond_wait(&new_package,&cond_lock);
+			printf("here\n");
+			empty = 0;
+			if (eof){			
+				return NULL;
+			}
+		}
+		pthread_mutex_lock(&llist_lock);
+		//pull package from llist
+		char *package = "hello.this.is.a.a.a.test test test test\n";
+		empty = 1;
+		pthread_mutex_unlock(&llist_lock);
+
+		//do word count on the package
+		int temp[26];
+		for (int i =0; i < 26; i++){
+			temp[i] = 0;
+		}
+		count_words(package, temp);
+
+		//add results to histogram
+		pthread_mutex_lock(&totals_lock);
+		for (int i =0; i < 26; i++){
+			totals[i] += temp[i];
+		}
+		pthread_mutex_unlock(&totals_lock);
+	}
+	
+
+}
+
+void *producer(void *args){
+	sleep(1);
+	pthread_cond_signal(&new_package);
+	sleep(1);
+	printf("producer finished\n");
+	eof = 1;
+	pthread_cond_broadcast(&new_package);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
